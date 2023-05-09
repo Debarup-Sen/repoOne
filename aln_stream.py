4b1878afdd7ac8dddf504bc09b20a7ccca00fe08
Debarup-Sen <79254755+Debarup-Sen@users.noreply.github.com>
2023-05-09 10:35:49 +0530

    Update aln_stream.py

--- aln_stream.py
+++ aln_stream.py
@@ -114,8 +114,8 @@ if tool == 'MUSCLE':
 
 if tool == 'Needleman-Wunsch':
     lit.text("FASTA format, plain text sequence format supported.")
-    query = lit.text_area('Enter your query sequence here:').upper()
-    subject = lit.text_area('Enter your subject sequence here:').upper()
+    query = myquery = lit.text_area('Enter your query sequence here:').upper()
+    subject = mysubject = lit.text_area('Enter your subject sequence here:').upper()
     submit = lit.button('Submit')
     if query and subject and submit:
         if '>' in query:
@@ -175,7 +175,7 @@ if tool == 'Needleman-Wunsch':
             lit.markdown('''<br>''', unsafe_allow_html=True)
             lit.text("Full alignment:")
             lines = open('NWFile').readlines()
-            lit.write('>'+query+'\n'+lines[0]+'>'+subject+'\n'+lines[1])
+            lit.write('>'+myquery+'\n'+lines[0]+'>'+mysubject+'\n'+lines[1])
             #lit.write(open('NWFile').read())
             lit.text("Score: "+str(score))
             open('NWFile', 'w').write('AMPDB Needleman-Wunsch Output:\n\nAlignment\n')
